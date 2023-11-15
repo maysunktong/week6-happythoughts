095205fcebc79153463a13d319a989dee16d160b
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2023-11-15 19:53:25 +0100

    Style PostBoard

--- src/components/PostBoard.jsx
+++ src/components/PostBoard.jsx
@@ -1,31 +1,35 @@
-import { useEffect, useState } from 'react';
-import api from '../utils/api';
+import { useEffect, useState } from "react";
+import api from "../utils/api";
+import giftImage from "../assets/gift.png";
+import gift2Image from "../assets/gift-2.png";
+import hatImage from "../assets/hat.png";
+import christmasTreeImage from "../assets/christmas-tree.png";
 
 export const PostBoard = () => {
   const [thoughts, setThoughts] = useState([]);
-  const [newThoughts, setNewThoughts] = useState('');
-  
+  const [newThoughts, setNewThoughts] = useState("");
+
   useEffect(() => {
     handleFetchThoughts();
   }, []);
 
-  const handleFetchThoughts = async() => {
+  const handleFetchThoughts = async () => {
     try {
       const data = await api.fetchThoughts();
       setThoughts(data);
     } catch (error) {
-      console.error('Error fetching thoughts:', error);
+      console.error("Error fetching thoughts:", error);
     }
-  }
+  };
 
   const handlePostThought = async () => {
     try {
       await api.postThought(newThoughts);
       handleFetchThoughts();
       // Clear the input field
-      setNewThoughts('');
+      setNewThoughts("");
     } catch (error) {
-      console.error('Error posting thought:', error);
+      console.error("Error posting thought:", error);
     }
   };
 
@@ -35,7 +39,7 @@ export const PostBoard = () => {
       // If the like is successful, fetch the latest thoughts
       handleFetchThoughts();
     } catch (error) {
-      console.error('Error liking thought:', error);
+      console.error("Error liking thought:", error);
     }
   };
 
@@ -43,32 +47,46 @@ export const PostBoard = () => {
     setNewThoughts(event.target.value);
   };
 
-
   return (
-    <div>
-      <h1>Post Board</h1>
-      <div>
-        <textarea
-          rows="4"
-          cols="50"
-          placeholder="Share your thoughts..."
-          value={newThoughts}
-          onChange={handleNewThoughtChange}
-        ></textarea>
+    <div className="flex justify-center items-center flex-col">
+      <h1>Happy Christmas Thoughts</h1>
+      <div className='flex justify-center items-center'>
+        <img src={christmasTreeImage} alt="" width={32} height={32} className='w-48' />
+        <div className='grid'> 
+          <textarea
+            rows="4"
+            cols="50"
+            placeholder="Share your positive Christmas message here..."
+            value={newThoughts}
+            onChange={handleNewThoughtChange}
+            className='bg-green-800 border-white border-4 shadow-inner shadow-black p-4 text-white'
+          ></textarea>
+          <button onClick={handlePostThought}>Post Thought</button>
+        </div>
+        <img src={gift2Image} alt="" width={32} height={32} className='w-48' />
         <br />
-        <button onClick={handlePostThought}>Post Thought</button>
+        
       </div>
-      <div>
-        <h2>Latest Thoughts</h2>
-        <ul className='flex w-8'>
+      <div className="flex justify-center items-center w-[80%]">
+        <ul className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-12">
           {thoughts.map((thought) => (
-            <li key={thought._id} className='w-fit'>
-              {thought.message} - Likes: {thought.hearts}
-              <button onClick={() => handleLikeThought(thought._id)}>Like</button>
+            <li
+              key={thought._id}
+              className="border-white border-4 shadow-inner shadow-black bg-yellow-200 rounded-xl p-6 break-words"
+            >
+              <span className="block h-[8rem] text-lg md:text-base sm:text-sm  text-gray-800">
+                {thought.message}
+              </span>
+              <div className="flex justify-end items-center">
+                <span className="px-2 text-red-600">{thought.hearts}</span>
+                <button onClick={() => handleLikeThought(thought._id)}>
+                  <img src={giftImage} alt="" width={32} height={32} />
+                </button>
+              </div>
             </li>
           ))}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
