a7c6f0cbcfdde2fed3166d2ddde02be8b315f895
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2023-11-15 21:54:54 +0100

    Style PostBoard

--- src/components/PostBoard.jsx
+++ src/components/PostBoard.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
-import api from "../utils/api";
-import giftImage from "../assets/gift.png";
-import gift2Image from "../assets/gift-2.png";
-import hatImage from "../assets/hat.png";
 import christmasTreeImage from "../assets/christmas-tree.png";
+import gift2Image from "../assets/gift-2.png";
+import giftImage from "../assets/gift.png";
+import api from "../utils/api";
 
 export const PostBoard = () => {
   const [thoughts, setThoughts] = useState([]);
   const [newThoughts, setNewThoughts] = useState("");
+  const [isTextTooLong, setIsTextTooLong] = useState(false);
 
   useEffect(() => {
     handleFetchThoughts();
@@ -23,13 +23,20 @@ export const PostBoard = () => {
   };
 
   const handlePostThought = async () => {
-    try {
-      await api.postThought(newThoughts);
-      handleFetchThoughts();
-      // Clear the input field
-      setNewThoughts("");
-    } catch (error) {
-      console.error("Error posting thought:", error);
+    if (!isTextTooLong) {
+      try {
+        await api.postThought(newThoughts);
+        // If the thought is posted successfully, fetch the latest thoughts
+        handleFetchThoughts();
+        // Clear the input field
+        setNewThoughts("");
+        // Reset the isTextTooLong state
+        setIsTextTooLong(false);
+      } catch (error) {
+        console.error("Error posting thought:", error);
+      }
+    } else {
+      console.error("Text is too long. Cannot post thought.");
     }
   };
 
@@ -44,37 +51,63 @@ export const PostBoard = () => {
   };
 
   const handleNewThoughtChange = (event) => {
-    setNewThoughts(event.target.value);
+    const newText = event.target.value;
+    setNewThoughts(newText);
+    setIsTextTooLong(newText.length > 140);
   };
 
   return (
     <div className="flex justify-center items-center flex-col">
-      <h1>Happy Christmas Thoughts</h1>
-      <div className='flex justify-center items-center'>
-        <img src={christmasTreeImage} alt="" width={32} height={32} className='w-48' />
-        <div className='grid'> 
-          <textarea
-            rows="4"
-            cols="50"
-            placeholder="Share your positive Christmas message here..."
-            value={newThoughts}
-            onChange={handleNewThoughtChange}
-            className='bg-green-800 border-white border-4 shadow-inner shadow-black p-4 text-white'
-          ></textarea>
-          <button onClick={handlePostThought}>Post Thought</button>
+      <h1 className="lg:text-8xl md:text-6xl sm:text-4xl font-festive text-white">
+        Happy Christmas Thoughts
+      </h1>
+      <div className="flex justify-center items-center py-8">
+        <img
+          src={christmasTreeImage}
+          alt=""
+          width={32}
+          height={32}
+          className="lg:w-48 md:w-32 sm:w-24"
+        />
+        <div className="grid">
+          <div className="relative">
+            <textarea
+              rows="4"
+              cols="50"
+              placeholder="Share your positive Christmas message here..."
+              value={newThoughts}
+              onChange={handleNewThoughtChange}
+              className="max-h-[10rem] w-full bg-green-200 border-white border-4 shadow-inner shadow-black p-4 text-white"
+              style={{ color: isTextTooLong ? "red" : "black" }}
+            ></textarea>
+            <div className="absolute bottom-6 right-4">
+              <span className={`text-${isTextTooLong ? "red" : "green"}-600`}>
+                {newThoughts.length <= 140
+                  ? `${140 - newThoughts.length} words remaining`
+                  : `-${newThoughts.length - 140} words exceeding`}
+              </span>
+            </div>
+          </div>
+          <button
+            onClick={handlePostThought}
+            disabled={isTextTooLong}
+            style={isTextTooLong ? { backgroundColor: "grey", cursor: "not-allowed" } : {}}
+            className="w-fit p-4 border-white border-2 bg-pink-400 m-auto rounded-full text-white"
+          >
+            Post Thought
+          </button>
         </div>
-        <img src={gift2Image} alt="" width={32} height={32} className='w-48' />
+        <img src={gift2Image} alt="" width={32} height={32} className="lg:w-48 md:w-32 sm:w-24" />
         <br />
-        
       </div>
       <div className="flex justify-center items-center w-[80%]">
-        <ul className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-12">
+        <ul className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-12 md:gap-4">
           {thoughts.map((thought) => (
             <li
               key={thought._id}
-              className="border-white border-4 shadow-inner shadow-black bg-yellow-200 rounded-xl p-6 break-words"
+              className="h-full border-white border-4 shadow-inner shadow-black bg-yellow-200 rounded-xl p-6 break-words"
             >
-              <span className="block h-[8rem] text-lg md:text-base sm:text-sm  text-gray-800">
+              <span className="block text-lg md:text-base sm:text-sm  text-gray-800">
                 {thought.message}
               </span>
               <div className="flex justify-end items-center">
